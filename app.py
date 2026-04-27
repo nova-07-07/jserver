f1ba0380284e1e472aadbd3517a109c88b29819e
nova-07-07 <snova234607ns@gmail.com>
2026-04-27 16:48:38 +0530

    add requirements

--- app.py
+++ app.py
@@ -207,5 +207,8 @@ def delete_row():
     return "OK"
 
 # ================= RUN =================
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
